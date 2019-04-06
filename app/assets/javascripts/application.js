3b72bf23b81db78c356e44c7b539d7f019d5b3f0
Esteban <estebanmh95@gmail.com>
2019-04-05 20:17:56 -0500

    Ticket #1 - Linking CSS and JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,26 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+//= jquery/jquery-migrate.js
+//= jquery-ui/jquery-ui.min.js
+//= smoothscroll/SmoothScroll.min.js
+//= waypoints/waypoints.min.js
+//= counter-up/jquery.counterup.min.js
+//= datepicker/bootstrap-datepicker.min.js
+//= selectbox/jquery.selectbox-0.1.3.min.js
+//= owl-carousel/owl.carousel.min.js
+//= isotope/isotope.min.js
+//= fancybox/jquery.fancybox.min.js
+//= isotope/isotope-triger.min.js
+//= rateyo/jquery.rateyo.min.js
+//= map/js/markerclusterer.js
+//= map/js/rich-marker.js
+//= map/js/infobox_packed.js
+
+//= map.js
+//= app.js
+
 //= require_tree .
