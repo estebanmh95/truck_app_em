8a5c6487110964a62ae3016e7d3b7de421227516
Esteban <estebanmh95@gmail.com>
2019-04-09 20:18:10 -0500

    Ticket #2 - Blog Scaffold

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :validate_user
+  before_action :find_user, only: [:show]
+
+  def show
+    avatar_url = current_user.avatar_url
+    @large_avatar = "#{avatar_url}&s=170"
+  end
+
+  private
+    def validate_user
+      if current_user.id.to_s != params[:id]
+        redirect_to root_path
+        flash[:alert] = "You don't have permissions."
+      end
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :articles
   devise_for :users
+  resources :users, only: [:show]
+
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
