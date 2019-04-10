5c2771a4aa2fb60001509047e735373f71f9f0c6
Esteban <estebanmh95@gmail.com>
2019-04-09 19:08:34 -0500

    Ticket #2 - Add Role Column to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  #add parameters/column to Devise
+  before_action :add_parameters_to_user, if: :devise_controller?
+  before_action :avatar
+
+  protected
+
+    def add_parameters_to_user
+      devise_parameter_sanitizer.permit(:sign_up,  keys: [:role])
+      devise_parameter_sanitizer.permit(:account_update,  keys: [:role])
+    end
+
+    def avatar
+      @avatar = user_signed_in? ? current_user.avatar_url : nil
+    end
+
 end
