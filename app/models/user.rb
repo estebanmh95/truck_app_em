a02e99611ee27bc1e2c038e9e431897f516649f3
Esteban <estebanmh95@gmail.com>
2019-04-16 08:26:05 -0500

    Ticket #2 - Adicionar Tabla Usuario y Articulo y Controlador Articulos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :articles
   enum role: [ :user, :shipper, :trucker, :admin]
 
   before_create :set_default_user
