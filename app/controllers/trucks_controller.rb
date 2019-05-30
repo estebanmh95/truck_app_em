71b702e79d5993903908159372a8c3b0dd2fb2f2
Esteban <estebanmh95@gmail.com>
2019-05-30 13:36:30 -0500

    Ticket #2 - New Truck Form

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,45 +1,65 @@
 class TrucksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :is_user?, except: [:index, :show]
+  before_action :set_truck, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @trucks = Truck.all
   end
 
+  def show
+
+  end
+
   def new
-      @truck = Truck.new
+    @truck = Truck.new
   end
 
   def create
     @truck = Truck.new(truck_params)
-    respond_to do |format|
-      if @truck.save
-        format.html { redirect_to @truck, notice: 'Truck was successfully created.' }
-        format.json { render :show, status: :created, location: @truck }
+    @truck.user = current_user
 
-      else
-        format.html { render :new }
-        format.json { render json: @truck.errors, status: :unprocessable_entity }
-      end
+    if @truck.save
+      redirect_to articles_path, notice: 'Truck was successfully created.'
+    else
+      render :new
     end
   end
 
   def edit
+
+  end
+
+  def update
+    if @truck.update(truck_params)
+      redirect_to articles_path, notice: 'Truck was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @truck.destroy
+    redirect_to trucks_url, notice: 'Truck was successfully destroyed.'
   end
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def truck_params
-      params.require(:truck).permit(:gps, :available_now,:image, :latitude, :longitude,
-        :price_per_km, :user_id, :truck_type_id, :truck_merchandise_id,:truck_load_id,
-        :coverage_id, :city_id )
-    end
+  def set_truck
+    @truck = Truck.find(params[:id])
+  end
 
-    def is_user?
-      unless current_user
-        flash[:alert] = "You don't have permissions"
-        redirect_to trucks_path
-      end
+
+  def truck_params
+    params.require(:truck).permit(:user_id, :truck_type_id, :truck_merchandise_id, :truck_load_id, :coverage_id, :city_id, :gps, :available_now, :image, :latitude, :longitude, :price_per_km)
+  end
+
+  def is_user?
+    unless current_user
+      flash[:alert] = "You don't have permissions"
+      redirect_to trucks_path
     end
+  end
+
 end
